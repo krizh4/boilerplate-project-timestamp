ea2c68e2383327030b443656e075523b0095a2a2
krizh4 <krishmikaabisheka1@gmail.com>
2022-09-06 17:42:22 +0530

    patch_update

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  res.json({
+    unix: new Date().valueOf(),
+    utc: new Date().toUTCString()
+  });
 });
 
 let output;
